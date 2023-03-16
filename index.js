912fc14c7abde886aaf1dc45f614eee2f5fc7bd2
gustavobetete <gustavobetete@hotmail.com>
2023-03-16 10:06:01 -0300

    feat: index com todas as classes

---
+++ index.js
@@ -0,0 +1,14 @@
+import User from "./User.js";
+import Admin from "./Admin.js";
+import Docente from "./Docente.js";
+
+const novoUser = new User('Gustavo', 'gustavo@email.com', '12/05/1999')
+const novoAdmin = new Admin('Ana Beatriz', 'ana@email.com', '13/04/1999')
+const novoDocente = new Docente('Yhonathan', 'yho@email.com', '25/01/2000')
+
+console.log(novoUser.exibirInfos())
+console.log(novoAdmin.exibirInfos())
+console.log(novoDocente.exibirInfos())
+
+// novoUser.#nome = 'Gabriel'
+// console.log(novoUser.#nome) // campo privado, não deixa trocar
