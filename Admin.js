3f8af1dd4423ddb8170c7f36659ab6c29edf921f
gustavobetete <gustavobetete@hotmail.com>
2023-03-16 09:44:17 -0300

    feat: adicionando nova função para admin

--- Admin.js
+++ Admin.js
@@ -4,17 +4,11 @@ class Admin extends User{
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
         super(nome, email, nascimento, role, ativo);
     }
+
+    criarCurso(nomeCurso, vagas){
+        return `Curso de ${nomeCurso} criado com ${vagas} vagas`
+    }
 }
 
 const novoAdmin = new Admin('Rodrigo', 'r@r.com', '1999-01-20')
-console.log(novoAdmin)
-/*
-  Admin {
-  nome: 'Rodrigo',
-  email: 'r@r.com',
-  nascimento: '1999-01-20',
-  role: 'admin',
-  ativo: true
-}*/
-
-console.log(novoAdmin.exibirInfos()) //Rodrigo, r@r.com
+console.log(novoAdmin.criarCurso('Desenvolvimento Web Full stack', 20))
