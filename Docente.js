e822855cf79742d913f2f697e7846bb2580f7365
gustavobetete <gustavobetete@hotmail.com>
2023-03-16 09:43:50 -0300

    feat: criando classe Docente (Filho)

---
+++ Docente.js
@@ -0,0 +1,16 @@
+import User from "./User.js";
+// Classe filho
+class Docente extends User{
+    constructor(nome, email, nascimento, role = 'docente', ativo = true) {
+        super(nome, email, nascimento, role, ativo);
+    }
+
+    aprovarEstudante(estudante, curso){
+       return `O estudante ${estudante} foi aprovado(a) no curso ${curso}. `
+    }
+}
+
+const novoDocente = new Docente('Guilherme', 'g@gmail.com', '12/05/1984')
+console.log(novoDocente)
+console.log(novoDocente.exibirInfos())
+console.log(novoDocente.aprovarEstudante('Guilherme', 'Análise e desenvolvimento de sistemas'))
